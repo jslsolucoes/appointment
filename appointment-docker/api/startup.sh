7193d46d0419ee3254cb1cf6722ff3429a036e9c
Jonatan Lemes <jonatan@jslsolucoes.com>
2021-01-25 01:35:44 -0300

    support docker

---
+++ appointment-docker/api/startup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /opt/appointment/appointment-api/target && java -jar appointment-api-0.0.1-SNAPSHOT.jar
---
+++ appointment-docker/bot/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sleep 15
+cd /opt/appointment/appointment-bot/target && java -jar appointment-bot-0.0.1-SNAPSHOT.jar
